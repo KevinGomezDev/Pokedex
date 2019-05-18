386ecc54c74ecd0188b6de987630144c8f5adad7
Kevin Gomez <dev.kevingomez@gmail.com>
2019-05-18 11:17:02 -0500

    Basic Pokedex Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'nes.css/scss/nes.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='pokedex'>
+      <div className='pokedex-frame'>
+        <div className='pokedex-menu'>Menu</div>
+        <div className='pokedex-controls'>Controls</div>
+      </div>
+      <div className='pokedex-content'>Content</div>
     </div>
   );
 }
