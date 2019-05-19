30ca6ca96cab997a07c80ac485380117becdc763
Kevin Gomez <dev.kevingomez@gmail.com>
2019-05-19 13:58:53 -0500

    Cleaning Up layout

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import initialState from './reducers/initialState';
 
-import './App.css';
-import 'nes.css/scss/nes.scss';
-
-import Home from './containers/Home';
-import Login from './containers/Login';
-import Pokemon from './views/Pokemon';
-import SignUp from './containers/SignUp';
+import MainLayout from './components/MainLayout';
 
 const store = configureStore(initialState);
 
 function App() {
-  return  <Provider store={store}>
+  return <Provider store={store}>
     <Router> 
-    <div className='pokedex'>
-      <div className='pokedex-frame'>
-        <div className='pokedex-menu'>
-          <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/login">Login</NavLink>
-          </li>
-          <li>
-            <NavLink to="/sign-up">Sign-Up</NavLink>
-          </li>
-        </ul>
-        </div>
-        <div className='pokedex-controls'>Controls</div>
-      </div>
-      <div className='pokedex-content'>
-      <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/sign-up" component={SignUp} />
-        <Route exact path="/pokemon/:id" component={Pokemon} />
-      </div>
-  </div>
-  </Router>
+      <MainLayout />
+    </Router>
   </Provider>
 }
 
---
+++ src/components/MainLayout.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Route, NavLink } from 'react-router-dom';
+
+import '../App.css';
+import 'nes.css/scss/nes.scss';
+
+import Home from '../containers/Home';
+import Login from '../containers/Login';
+import Pokemon from '../views/Pokemon';
+import SignUp from '../containers/SignUp';
+
+function MainLayout () {
+  return <div className='pokedex'>
+    <div className='pokedex-frame'>
+      <div className='pokedex-menu'>
+        <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/login">Login</NavLink></li>
+        <li><NavLink to="/sign-up">Sign-Up</NavLink></li>
+      </ul>
+      </div>
+      <div className='pokedex-controls'>Controls</div>
+    </div>
+    <div className='pokedex-content'>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/sign-up" component={SignUp} />
+      <Route exact path="/pokemon/:id" component={Pokemon} />
+    </div>
+  </div>
+}
+
+export default MainLayout
